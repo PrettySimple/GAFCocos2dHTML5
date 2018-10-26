a3ce083d17e6d6c588a8ca7a6c4b33475f5abd02
sebastienalcamo <sebastiena@prettysimplegames.com>
2018-10-26 16:50:41 +0200

    Invalidates the Director's dirty flag when a GAF Object's visibility is updated (fixes all GAFs bugs)

--- Library/GAFObject.js
+++ Library/GAFObject.js
@@ -295,10 +295,21 @@ gaf.Object = cc.Node.extend
             this.setOpacity(0);
     },
 
+    // @Override (bug fix)
+    setOpacity: function (opacity)
+    {
+        // If the node is about to be displayed or undisplayed.. we invalidate the renderer's children order
+        // It will force a visit() on all scenes' nodes. Required to render or unrender the GAF Object
+        // Fixes broken GAF Anims (occurs when we do not visit invisible GAF Objects)
+        if ((this._realOpacity == 0 && opacity > 0) || (this._realOpacity > 0 && opacity == 0))
+            cc.renderer.childrenOrderDirty = true;
+        this._super(opacity);
+    },
+
     // @Override
     visit: function(parentCmd)
     {
-        // if(this.isVisible())     // Loses optim but fixes some edge cases that would make some GAF animations not work properly!
+        if(this.isVisible())
         {
             this._super(parentCmd);
         }
